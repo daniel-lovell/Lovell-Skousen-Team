3ccdb43f06e2c452796b8091600b9ac5e294a1cd
Daniel Lovell <daniel.lovell@me.com>
2015-01-30 00:18:55 -0700

    Added Dragon, ExperienceMedallion, Location, SkillsScene, SupplyScene classes

--- hobbit/src/hobbit/Hobbit.java
+++ hobbit/src/hobbit/Hobbit.java
@@ -6,6 +6,10 @@
 package hobbit;
 
 import byui.cit260.hobbit.model.Player;
+import byui.cit260.hobbit.model.Game;
+import byui.cit260.hobbit.model.Dragon;
+import byui.cit260.hobbit.model.ExperienceMedallion;
+import byui.cit260.hobbit.model.Location;
 
 /**
  *
@@ -17,7 +21,8 @@ public class Hobbit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+        
+        //Test Player Class
         Player player = new Player();
         
         player.setName("Bob");
@@ -25,6 +30,44 @@ public class Hobbit {
         
         String playerInfo = player.toString();
         System.out.println(playerInfo);
+        
+        //Test Game Class
+        Game game = new Game();
+        
+        game.setLastSaved(1422586253);
+        game.setCreated(1422586253);
+        
+        String gameInfo = game.toString();
+        System.out.println(gameInfo);
+        
+        //Test Dragon Class
+        Dragon dragon = new Dragon();
+        
+        dragon.setLifePoints(22);
+        
+        String dragonInfo = dragon.toString();
+        System.out.println(dragonInfo);
+        
+        //Test ExperienceMedallion Class
+        ExperienceMedallion experienceMedallion = new ExperienceMedallion();
+        
+        experienceMedallion.setAmountAttack(4);
+        experienceMedallion.setMaxAmountAttack(8);
+        experienceMedallion.setAmountDefend(5);
+        experienceMedallion.setMaxAmountDefend(9);
+        
+        String experienceMedallionInfo = experienceMedallion.toString();
+        System.out.println(experienceMedallionInfo);
+        
+        //Test Location Class
+        Location location = new Location();
+        
+        location.setRowCoordinate(2);
+        location.setColumnCoordinate(3);
+        
+        String locationInfo = location.toString();
+        System.out.println(locationInfo);
+        
     }
     
 }
